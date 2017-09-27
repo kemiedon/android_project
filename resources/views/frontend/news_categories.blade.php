910fa1a286b23ca129838b2b9c05d361041f1cbf
Sheng-Bo Chen <artalose@gmail.com>
2017-09-26 21:04:32 -0400

    2017.09.27.001 前台修正

--- resources/views/frontend/news_categories.blade.php
+++ resources/views/frontend/news_categories.blade.php
@@ -17,35 +17,30 @@
     <script src="{{ asset('_libs/tinymce/tinymce.min.js') }}" type="text/javascript"></script>
     <script src="{{ asset('_libs/bootstrap/dist/js/bootstrap.min.js') }}" type="text/javascript"></script>
 
-
-
 </head>
 
-
-
 <body>
+    <div class="container-fluid" style="background-color:#FFFFFF;">
 
-
-    <!-- // mv_FR1C2_start -->
-    <?php $news_category = DB::table('news_categories')->find(1); ?>
-    <style>
-        #FR1C2_Left {
-            background-color: #f6f6f6;
-            padding-top: 70px;
-            padding-bottom: 70px;
-            color: #999;
-            background-image: url('{{ asset("images/img_placeholder.jpg") }}');
-            background-repeat: no-repeat;
-            background-position: center;
-            background-size: cover;
-        }
-        
-        #FR1C2_Right {
-            padding-top: 70px;
-            padding-bottom: 70px;
-        }
-    </style>
-    <div class="container-fluid">
+        <!-- // mv_FR1C2_start -->
+        <?php $news_category = DB::table('news_categories')->find(1); ?>
+        <style>
+            #FR1C2_Left {
+                background-color: #f6f6f6;
+                padding-top: 70px;
+                padding-bottom: 70px;
+                color: #999;
+                background-image: url('{{ asset("images/img_placeholder.jpg") }}');
+                background-repeat: no-repeat;
+                background-position: center;
+                background-size: cover;
+            }
+            
+            #FR1C2_Right {
+                padding-top: 70px;
+                padding-bottom: 70px;
+            }
+        </style>
         <div class="row">
             <div id="FR1C2_Left" class="col-sm-12 col-md-6 text-center">
                 形象圖位置
@@ -59,100 +54,99 @@
 
             </div>
         </div>
-    </div>
-    <script>
-        $(function() {
-            $('#FR1C2_Left').height($('#FR1C2_Right').height());
-        })
-    </script>
-    <!-- // mv_FR1C2_end -->
+        <script>
+            $(function() {
+                $('#FR1C2_Left').height($('#FR1C2_Right').height());
+            })
+        </script>
+        <!-- // mv_FR1C2_end -->
 
-    <p>&nbsp;</p>
+        <p>&nbsp;</p>
 
-    <!-- // mv_XR1C2_start -->
-    <style>
-        #XR1C2_Left {
-            background-color: #f6f6f6;
-            padding-top: 40px;
-            padding-bottom: 40px;
-            color: #999;
-            background-image: url('{{ asset("images/img_placeholder.jpg") }}');
-            background-repeat: no-repeat;
-            background-position: center;
-            background-size: cover;
-            /* border-left: 15px solid #fff; */
-        }
-        
-        #XR1C2_Right {
-            /* background-color: #f6f6f6; */
-            padding-top: 40px;
-            padding-bottom: 40px;
-            /* border-right: 15px solid #fff; */
-        }
-    </style>
-    <div class="container">
+
+        <!-- // mv_XR1C2_start -->
+        <style>
+            #XR1C2_Left {
+                background-color: #f6f6f6;
+                padding-top: 40px;
+                padding-bottom: 40px;
+                color: #999;
+                background-image: url('{{ asset("images/img_placeholder.jpg") }}');
+                background-repeat: no-repeat;
+                background-position: center;
+                background-size: cover;
+                /* border-left: 15px solid #fff; */
+            }
+            
+            #XR1C2_Right {
+                /* background-color: #f6f6f6; */
+                padding-top: 40px;
+                padding-bottom: 40px;
+                /* border-right: 15px solid #fff; */
+            }
+        </style>
         <div class="row">
-            <div id="XR1C2_Left" class="col-sm-12 col-md-6 text-center">
+            <div id="XR1C2_Left" class="col-sm-12 col-md-4 col-md-offset-2 text-center">
                 形象圖位置
             </div>
-            <div id="XR1C2_Right" class="col-sm-12 col-md-6">
+            <div id="XR1C2_Right" class="col-sm-12 col-md-4">
                 <div class="col-md-10 col-md-offset-1">
                     <h2 style="margin-bottom: 1.25em;">{{ $news_category->name }}</h2>
                     <span class="text-justify">{!! $news_category->description !!}</span>
                 </div>
             </div>
         </div>
-    </div>
-    <script>
-        $(function() {
-            $('#XR1C2_Left').height($('#XR1C2_Right').height());
-        })
-    </script>
-    <!-- // mv_XR1C2_end -->
+        <script>
+            $(function() {
+                $('#XR1C2_Left').height($('#XR1C2_Right').height());
+            })
+        </script>
+        <!-- // mv_XR1C2_end -->
 
-    <p>&nbsp;</p>
+        <p>&nbsp;</p>
 
-    <!-- // mv_XR1C4_start -->
-    <?php $news_categories = DB::table('news_categories')->get(); ?>
-    <style>
-        .XR1C4>div:first-child {
-            background-color: #f6f6f6;
-            height: 150px;
-            color: #999;
-            background-image: url('{{ asset("images/img_placeholder.jpg") }}');
-            background-repeat: no-repeat;
-            background-position: center;
-            background-size: cover;
-        }
-        
-        @media(min-width:992px) {
-            .XR1C4:first-child {
-                padding-left: 0px;
+        <!-- // mv_XR1C4_start -->
+        <?php $news_categories = DB::table('news_categories')->get(); ?>
+        <style>
+            .XR1C4>div:first-child {
+                background-color: #f6f6f6;
+                height: 150px;
+                color: #999;
+                background-image: url('{{ asset("images/img_placeholder.jpg") }}');
+                background-repeat: no-repeat;
+                background-position: center;
+                background-size: cover;
             }
-            .XR1C4:nth-child(4n) {
-                padding-right: 0px;
+            
+            @media(min-width:992px) {
+                .XR1C4:first-child {
+                    padding-left: 0px;
+                }
+                .XR1C4:nth-child(4n) {
+                    padding-right: 0px;
+                }
             }
-        }
-    </style>
-    <div class="container">
-        <h2 class="col-md-12 text-center" style="margin-bottom: 1.25em;">營業項目</h2>
+        </style>
 
         <div class="row">
-            @foreach($news_categories as $news_category)
-            <div class="col-md-3 XR1C4">
-                <div class="text-center">圖片位置</div>
-                <h3 class="text-left">{{ $news_category->name }}</h3>
-                <span class="text-justify">{!! substr($news_category->description, 3, 253) !!}</span>
-            </div>
-            @endforeach
+            <h2 class="col-md-12 text-center" style="margin-bottom: 1.25em;">營業項目</h2>
 
+            <div class="col-md-offset-2">
+
+                @foreach($news_categories as $news_category)
+                <div class="col-md-2 XR1C4">
+                    <div class="text-center">圖片位置</div>
+                    <h3 class="text-left">{{ $news_category->name }}</h3>
+                    <span class="text-justify">{!! substr($news_category->description, 3, 253) !!}</span>
+                </div>
+                @endforeach
+            </div>
         </div>
 
+        <!-- // mv_XR1C4_end -->
+
+
     </div>
-    <!-- // mv_XR1C4_end -->
-
-
-
 </body>
 
 </html>
